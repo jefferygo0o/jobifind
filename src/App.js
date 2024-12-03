447591618fb6e2f160744474ec42232ac0cd43a8
jefferygo0o <111522055+jefferygo0o@users.noreply.github.com>
2024-12-03 02:14:34 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import Dashboard from "./pages/Dashboard";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import Dashboard from "../pages/Dashboard";
 
 function App() {
   return (

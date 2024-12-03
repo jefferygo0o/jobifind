f07d700f8e9222c7e6dff93752d1ac31fc9e2e84
jefferygo0o <111522055+jefferygo0o@users.noreply.github.com>
2024-12-03 02:32:45 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
+import LoginPage from './pages/LoginPage';
 
 const App = () => {
   return (

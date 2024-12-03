20104eb2ecce7493242144485dd11cc09c427f4f
jefferygo0o <111522055+jefferygo0o@users.noreply.github.com>
2024-12-03 15:02:21 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-import Dashboard from "./pages/Dashboard";
 import JobSearchPage from "./pages/JobSearchPage";
+import FavoritesPage from "./pages/FavoritesPage";
 
-function App() {
+const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/job-search" element={<JobSearchPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;

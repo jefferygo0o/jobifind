fc970ec443c2c44540706064ca4010139452ae1f
jefferygo0o <111522055+jefferygo0o@users.noreply.github.com>
2024-12-03 02:53:25 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-// src/App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import Dashboard from "./pages/Dashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage"; // Login page component
+import SignupPage from "./pages/SignupPage";       // Home page component
+import Dashboard from "./pages/Dashboard";    // Example dashboard page
 
-function MyApp() {
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <div>
+        {/* Define routes for your app */}
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
-export default MyApp;
+export default App;

177dcdc2cce7c33d84b570164f2a34d9303c17f9
jefferygo0o <111522055+jefferygo0o@users.noreply.github.com>
2024-12-03 01:26:18 +0000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const database = getDatabase(app);

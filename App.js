8090c3120394d26d16c43c125af6b8f884a1e9f4
jefferygo0o <111522055+jefferygo0o@users.noreply.github.com>
2024-12-03 01:26:56 +0000

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;

65d2b8adeea26c343bf8e2316474d82c354dd016
jefferygo0o <111522055+jefferygo0o@users.noreply.github.com>
2024-12-03 03:19:27 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // Ensure the App component is correctly imported
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );

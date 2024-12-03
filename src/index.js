23218f77a6b30ce9eb131bc8fd9b05000086ed49
jefferygo0o <111522055+jefferygo0o@users.noreply.github.com>
2024-12-03 04:04:58 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "../App";
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-    <div>Hello</div>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
